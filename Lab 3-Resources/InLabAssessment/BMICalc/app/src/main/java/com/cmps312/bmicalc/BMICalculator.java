fbb38176468c805b0c47749e32715faa12e0dc94
abdalabaaji <abdala.baaji@gmail.com>
2019-09-15 17:58:02 +0300

    updated

---
+++ Lab 3-Resources/InLabAssessment/BMICalc/app/src/main/java/com/cmps312/bmicalc/BMICalculator.java
@@ -0,0 +1,6 @@
+package com.cmps312.bmicalc;
+
+import android.app.Activity;
+
+public class BMICalculator extends Activity {
+}
---
+++ Lab 3-Resources/InLabAssessment/BMICalc/app/src/main/java/com/cmps312/bmicalc/WeightConverter.java
@@ -0,0 +1,6 @@
+package com.cmps312.bmicalc;
+
+import android.app.Activity;
+
+public class WeightConverter extends Activity {
+}
