33fe3d7dd18516e0fd50666ac230ee70de771c56
abdalabaaji <abdala.baaji@gmail.com>
2019-09-22 23:37:26 +0300

    Lab 4

--- Lab 4-Intents And Permissons/TutorialLab4/app/src/main/java/com/cmps312/tutoriallab4/MainActivity.java
+++ Lab 4-Intents And Permissons/TutorialLab4/app/src/main/java/com/cmps312/tutoriallab4/MainActivity.java
@@ -1,14 +1,20 @@
 package com.cmps312.tutoriallab4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    final int READ_EXTERNAL_STORAGE_CODE = 101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +23,70 @@ public class MainActivity extends AppCompatActivity {
 
     public void openSecondActivity(View view) {
 
-        Intent intent = new Intent(this, SecondActivity.class);
+        Intent intent = new Intent("com.cmps312.tutoriallab4.OpenSecondActivity");
         startActivity(intent);
     }
+
+    public void saveImageToSdCard(View view) {
+
+        //1. check if you have the permission
+        if( ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, "Permission already granted", Toast.LENGTH_SHORT).show();
+        }else{
+            requestUserPermission();
+        }
+    }
+
+    private void requestUserPermission() {
+        //1.
+
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+        }
+        else{
+            ActivityCompat
+                    .requestPermissions(this,
+                            new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                            READ_EXTERNAL_STORAGE_CODE);
+        }
+
+        //2.
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == READ_EXTERNAL_STORAGE_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Lab 4-Intents And Permissons/TutorialLab4/app/src/main/java/com/cmps312/tutoriallab4/SecondActivity.java
+++ Lab 4-Intents And Permissons/TutorialLab4/app/src/main/java/com/cmps312/tutoriallab4/SecondActivity.java
@@ -1,20 +1,55 @@
 package com.cmps312.tutoriallab4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+    int currentNumber;
+    TextView numberTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        numberTv = findViewById(R.id.number_tv);
+
+
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        currentNumber = intent.getIntExtra("currentNumber", 0);
+        numberTv.setText(currentNumber + "");
+        super.onNewIntent(intent);
     }
 
     public void sendEmail(View view) {
-        Toast.makeText(this, "I will be able to send", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"abdulahi@qu.edu.qa", "aya156789@qu.edu.qa"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "First Email Using Intents");
+        intent.putExtra(Intent.EXTRA_TEXT, "This is the body of the email you want to send");
+        intent.setData(Uri.parse("mailto:"));
+
+        startActivity(Intent.createChooser(intent, "Choose the email client"));
+        // setType message/rfc822
+
+
+    }
+
+    public void loadNextInteger(View view) {
+
+        currentNumber = Integer.parseInt(numberTv.getText().toString());
+        currentNumber++;
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("currentNumber", currentNumber);
+        startActivity(intent);
     }
 }
