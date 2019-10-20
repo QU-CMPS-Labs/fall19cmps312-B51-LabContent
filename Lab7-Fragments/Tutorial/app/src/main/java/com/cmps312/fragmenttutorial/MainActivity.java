d47919ac35b07c7dcf2e993eafe77354e8a88262
abdalabaaji <abdala.baaji@gmail.com>
2019-10-20 16:51:17 +0300

    update4:51

--- Lab7-Fragments/Tutorial/app/src/main/java/com/cmps312/fragmenttutorial/MainActivity.java
+++ Lab7-Fragments/Tutorial/app/src/main/java/com/cmps312/fragmenttutorial/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.fragment_holder, fragmentTwo)
+                .addToBackStack(null)
                 .commit();
     }
 }
