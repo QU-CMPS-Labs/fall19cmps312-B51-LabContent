25f44119cd3cb4780928fa90aebcb50dcfdaf375
abdalabaaji <abdala.baaji@gmail.com>
2019-10-20 15:31:16 +0300

    Lab7

--- Lab 4-Intents And Permissons/TutorialLab4/app/src/main/java/com/cmps312/tutoriallab4/MainActivity.java
+++ Lab 4-Intents And Permissons/TutorialLab4/app/src/main/java/com/cmps312/tutoriallab4/MainActivity.java
@@ -1,20 +1,23 @@
 package com.cmps312.tutoriallab4;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 public class MainActivity extends AppCompatActivity {
 
     final int READ_EXTERNAL_STORAGE_CODE = 101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,37 +33,50 @@ public class MainActivity extends AppCompatActivity {
     public void saveImageToSdCard(View view) {
 
         //1. check if you have the permission
-        if( ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "Permission already granted", Toast.LENGTH_SHORT).show();
-        }else{
+        } else {
             requestUserPermission();
         }
     }
 
     private void requestUserPermission() {
         //1.
+        final String [] permissions =  {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
 
-        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission Reason")
+                    .setMessage("I need this permission because of bla bla bla bla")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat
+                                    .requestPermissions(MainActivity.this, permissions, READ_EXTERNAL_STORAGE_CODE);
+                        }
+                    })
+                    .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).show();
 
-        }
-        else{
-            ActivityCompat
-                    .requestPermissions(this,
-                            new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
-                            READ_EXTERNAL_STORAGE_CODE);
-        }
+        } else
+            ActivityCompat.requestPermissions(this,permissions, READ_EXTERNAL_STORAGE_CODE);
 
-        //2.
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == READ_EXTERNAL_STORAGE_CODE){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == READ_EXTERNAL_STORAGE_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-            }
-            else{
+            } else {
                 Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
             }
         }
